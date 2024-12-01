0908562f58e333cb5f8bba065a1a78d724be8626
Adriana Orellana <adriana.orellana.torrico@gmail.com>
2024-12-01 15:30:42 +0100

    Moving computer vision main to main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-import threading
-import time
-
 from cv import *
-from kalman import *
+from path_planning import *
 
-# -------- Constants -------- #
+# -------- Computer vision constants -------- #
 CAMERA_ID = 0
 
 REAL_MAP_HEIGHT_CM = 84
@@ -15,6 +12,7 @@ MAP_MAX_WIDTH = 800
 
 PADDING_OBSTACLES = 30
 
+
 def main():
     # -------- Variables -------- #
     map_detection = False
@@ -24,18 +22,17 @@ def main():
 
     map_coords = []
     obstacles_contours = []
-
-    goal_coords = None
+    mask_obstacles = None
 
     thymio_found = False
     thymio_coords = []
     thymio_angle = None
+    goal_coords = None
 
     path = []
 
-    mask_obstacles = None
-
     camera = cv2.VideoCapture(CAMERA_ID)
+
     # -------- Main loop -------- #
     while True:
         ret, frame = camera.read()
@@ -56,69 +53,61 @@ def main():
             cv2.polylines(frame, [map_coords.astype(np.int32)], True, (0, 255, 0), 2)
 
             # Perspective transformation
-            pts2 = np.float32([[0, 0], [MAP_MAX_WIDTH, 0], [MAP_MAX_WIDTH, MAP_MAX_HEIGHT], [0, MAP_MAX_HEIGHT]])
-            matrix = cv2.getPerspectiveTransform(map_coords, pts2)
-            map_frame = cv2.warpPerspective(frame, matrix, (MAP_MAX_WIDTH, MAP_MAX_HEIGHT))
+            map_frame = fix_map_perspective(frame, map_coords, MAP_MAX_WIDTH, MAP_MAX_HEIGHT)
 
-            # Step 2: Detect the obstacles inside the map and the goal
+            # Detect the obstacles inside the map and the goal
             if obstacles_detection:
                 obstacles_contours, mask_obstacles, goal_coords = detect_obstacles_and_goal(map_frame, PADDING_OBSTACLES)
                 obstacles_detection = False
-            
-            if len(obstacles_contours) > 0:
-                draw_obstacles(map_frame, obstacles_contours)
 
-            if goal_coords:
-                draw_goal(map_frame, goal_coords)
-
+            # Every iteration check if thymio is detected and get its information
             thymio_found, thymio_coords, thymio_angle = detect_thymio(map_frame)
 
-            # Step 3: Path planning
-            if path_planning:
-                if thymio_found and goal_coords:
-                    obstacle_vertices = get_obstacle_vertices(obstacles_contours)
-                    path = compute_global_path(thymio_coords, goal_coords, obstacle_vertices, mask_obstacles)
-                else:
-                    print(f'It was not possible to detect the path planning. Thymio: {thymio_found}, Goal: {goal_coords}')
-                path_planning = False
 
-            # Step 4: Init the project
-            if start_motion:
-                print('Starting the project...')
-                thymio_coords_cm = convert_pixel_to_cm(thymio_coords, REAL_MAP_WIDTH_CM, REAL_MAP_HEIGHT_CM, MAP_MAX_WIDTH, MAP_MAX_HEIGHT)
-                print(f'Thymio coordinates in cm: {thymio_coords_cm}')
-                # ------> Important: Here you have the position, and angle of the thymio
+        # Step 2: Path planning
+        if path_planning:
+            if thymio_found and goal_coords:
+                obstacle_vertices = get_obstacle_vertices(obstacles_contours)
+                path = compute_global_path(thymio_coords, goal_coords, obstacle_vertices, mask_obstacles)
+            else:
+                print(f'No path found. Thymio: {thymio_found}, Goal: {goal_coords}')
+            path_planning = False
 
 
+        # Step 3: Start motion
+        if start_motion:
             if len(path) > 0:
-                draw_path(path, map_frame)
+                thymio_coords_cm = convert_pixel_to_cm(thymio_coords, REAL_MAP_WIDTH_CM, REAL_MAP_HEIGHT_CM, MAP_MAX_WIDTH, MAP_MAX_HEIGHT)
+                # ------> Important: Here you have the position, and angle of the thymio
+            else:
+                print('No path to follow')
 
-            # Reshape map before display it
-            map_frame = cv2.resize(map_frame, (500, 400))
+
+        # ---------- Display the frames ---------- #
+        if len(obstacles_contours) > 0:
+            draw_obstacles(map_frame, obstacles_contours)
+
+        if goal_coords:
+            draw_goal(map_frame, goal_coords)
+
+        if len(map_coords) == 4:
+            # map_frame = cv2.resize(map_frame, (500, 400))
             cv2.imshow('Map', map_frame)
 
-        if thymio_found:
-            cv2.putText(frame, f'Thymio (x,y): {thymio_coords}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 50), 3)
-            cv2.putText(frame, f'Thymio angle rad: {thymio_angle:.4f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 50), 3)
-            cv2.putText(frame, f'Thymio angle deg: {np.degrees(thymio_angle):.4f}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 50), 3)
-
-        cv2.imshow('frame', frame)
+        cv2.imshow('Frame', frame)
 
         # ---------- Keyboard options ---------- #
-        # 1. Detect the map, obstacles and goal
         if cv2.waitKey(1) & 0xFF == ord('m'):
+            print('Detecting map...')
             map_detection = True
 
-        # 2. Path planning
         if cv2.waitKey(1) & 0xFF == ord('p'):
+            print('Path planning...')
             path_planning = True
-            print('Key p pressed')
 
-        # 3. Start the project
         if cv2.waitKey(1) & 0xFF == ord('s'):
             start_motion = True
 
-        # 4. Quit the program
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Closing the program...')
             break
